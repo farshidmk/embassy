c7346d0370b85185bc011c5ea27fc85561d9392b
mehdirasa <mehdi.rasa@hotmail.com>
2023-09-27 12:48:35 +0330

    Color Changed, logo changed, fonts changed

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -47,122 +47,128 @@ const Login = () => {
   return (
     <>
       <Head title="Login" />
-      <Block className="nk-block-middle nk-auth-body  wide-xs">
-        <div className="brand-logo pb-4 text-center">
-          <Link to={process.env.PUBLIC_URL + "/"} className="logo-link">
-            <img className="logo-light logo-img logo-img-lg" src={Logo} alt="logo" />
-            <img className="logo-dark logo-img logo-img-lg" src={LogoDark} alt="logo-dark" />
-          </Link>
-        </div>
+      <div className="nk-app-root">
+        <div className="nk-wrap nk-wrap-nosidebar">
+          <div className="nk-content">
+            <Block className="nk-block-middle nk-auth-body wide-xs">
+              <div className="brand-logo pb-4 text-center">
+                <Link to={process.env.PUBLIC_URL + "/"} className="logo-link">
+                  <img className="logo-light logo-img logo-img-lg" src={Logo} alt="logo" />
+                  <img className="logo-dark logo-img logo-img-lg" src={LogoDark} alt="logo-dark" />
+                </Link>
+              </div>
 
-        <PreviewCard className="card-bordered" bodyClass="card-inner-lg">
-          <BlockHead>
-            <BlockContent>
-              <BlockTitle tag="h4">Sign-In</BlockTitle>
-              <BlockDes>
-                <p>Access panel using your email and password.</p>
-              </BlockDes>
-            </BlockContent>
-          </BlockHead>
-          {error && (
-            <div className="mb-3">
-              <Alert color="danger" className="alert-icon">
-                <Icon name="alert-circle" /> {error.message}
-              </Alert>
-            </div>
-          )}
-          <Form className="is-alter" onSubmit={handleSubmit(onFormSubmit)}>
-            <div className="form-group">
-              <div className="form-label-group">
-                <label className="form-label" htmlFor="default-01">
-                  Username
-                </label>
-              </div>
-              <div className="form-control-wrap">
-                <input
-                  type="text"
-                  id="default-01"
-                  {...register("username", { required: "This field is required" })}
-                  placeholder="Enter your username"
-                  className="form-control-lg form-control"
-                />
-                {errors.name && <span className="invalid">{errors.name.message}</span>}
-              </div>
-            </div>
-            <div className="form-group">
-              <div className="form-label-group">
-                <label className="form-label" htmlFor="password">
-                  password
-                </label>
-                {/* <Link className="link link-primary link-sm" to={`${process.env.PUBLIC_URL}/auth-reset`}>
+              <PreviewCard className="card-bordered" bodyClass="card-inner-lg">
+                <BlockHead>
+                  <BlockContent>
+                    <BlockTitle tag="h4">Sign-In</BlockTitle>
+                    <BlockDes>
+                      <p>Access panel using your email and password.</p>
+                    </BlockDes>
+                  </BlockContent>
+                </BlockHead>
+                {error && (
+                  <div className="mb-3">
+                    <Alert color="danger" className="alert-icon">
+                      <Icon name="alert-circle" /> {error.message}
+                    </Alert>
+                  </div>
+                )}
+                <Form className="is-alter" onSubmit={handleSubmit(onFormSubmit)}>
+                  <div className="form-group">
+                    <div className="form-label-group">
+                      <label className="form-label" htmlFor="default-01">
+                        Username
+                      </label>
+                    </div>
+                    <div className="form-control-wrap">
+                      <input
+                        type="text"
+                        id="default-01"
+                        {...register("username", { required: "This field is required" })}
+                        placeholder="Enter your username"
+                        className="form-control-lg form-control"
+                      />
+                      {errors.name && <span className="invalid">{errors.name.message}</span>}
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <div className="form-label-group">
+                      <label className="form-label" htmlFor="password">
+                        password
+                      </label>
+                      {/* <Link className="link link-primary link-sm" to={`${process.env.PUBLIC_URL}/auth-reset`}>
                   Forgot Code?
                 </Link> */}
-              </div>
-              <div className="form-control-wrap">
-                <span
-                  onClick={(ev) => {
-                    ev.preventDefault();
-                    setPassState(!passState);
-                  }}
-                  style={{ cursor: "pointer" }}
-                  className={`form-icon lg form-icon-right password-switch `}
-                >
-                  {passState ? (
-                    <Icon name="eye" className="password-icon icon-show"></Icon>
-                  ) : (
-                    <Icon name="eye-off" className="password-icon icon-hide"></Icon>
-                  )}
-                </span>
-                <input
-                  type={passState ? "text" : "password"}
-                  id="password"
-                  {...register("password", { required: "This field is required" })}
-                  placeholder="Enter your password"
-                  className={`form-control-lg form-control ${passState ? "is-hidden" : "is-shown"}`}
-                />
-                {errors.password && <span className="invalid">{errors.password.message}</span>}
-              </div>
-            </div>
-            <div className="form-group">
-              <Button size="lg" className="btn-block" type="submit" color="primary">
-                {isLoading ? <Spinner size="sm" color="light" /> : "Sign in"}
-              </Button>
-            </div>
-          </Form>
-          <div className="form-note-s2 text-center pt-4">
-            New on our platform? <Link to={`${process.env.PUBLIC_URL}/auth-register`}>Create an account</Link>
+                    </div>
+                    <div className="form-control-wrap">
+                      <span
+                        onClick={(ev) => {
+                          ev.preventDefault();
+                          setPassState(!passState);
+                        }}
+                        style={{ cursor: "pointer" }}
+                        className={`form-icon lg form-icon-right password-switch `}
+                      >
+                        {passState ? (
+                          <Icon name="eye" className="password-icon icon-show"></Icon>
+                        ) : (
+                          <Icon name="eye-off" className="password-icon icon-hide"></Icon>
+                        )}
+                      </span>
+                      <input
+                        type={passState ? "text" : "password"}
+                        id="password"
+                        {...register("password", { required: "This field is required" })}
+                        placeholder="Enter your password"
+                        className={`form-control-lg form-control ${passState ? "is-hidden" : "is-shown"}`}
+                      />
+                      {errors.password && <span className="invalid">{errors.password.message}</span>}
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <Button size="lg" className="btn-block" type="submit" color="primary">
+                      {isLoading ? <Spinner size="sm" color="light" /> : "Sign in"}
+                    </Button>
+                  </div>
+                </Form>
+                <div className="form-note-s2 text-center pt-4">
+                  New on our platform? <Link to={`${process.env.PUBLIC_URL}/auth-register`}>Create an account</Link>
+                </div>
+                <div className="text-center pt-4 pb-3">
+                  <h6 className="overline-title overline-title-sap">
+                    <span>OR</span>
+                  </h6>
+                </div>
+                <ul className="nav justify-center gx-4">
+                  <li className="nav-item">
+                    <a
+                      className="nav-link"
+                      href="#socials"
+                      onClick={(ev) => {
+                        ev.preventDefault();
+                      }}
+                    >
+                      Facebook
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      className="nav-link"
+                      href="#socials"
+                      onClick={(ev) => {
+                        ev.preventDefault();
+                      }}
+                    >
+                      Google
+                    </a>
+                  </li>
+                </ul>
+              </PreviewCard>
+            </Block>
           </div>
-          <div className="text-center pt-4 pb-3">
-            <h6 className="overline-title overline-title-sap">
-              <span>OR</span>
-            </h6>
-          </div>
-          <ul className="nav justify-center gx-4">
-            <li className="nav-item">
-              <a
-                className="nav-link"
-                href="#socials"
-                onClick={(ev) => {
-                  ev.preventDefault();
-                }}
-              >
-                Facebook
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className="nav-link"
-                href="#socials"
-                onClick={(ev) => {
-                  ev.preventDefault();
-                }}
-              >
-                Google
-              </a>
-            </li>
-          </ul>
-        </PreviewCard>
-      </Block>
+        </div>
+      </div>
       <AuthFooter />
     </>
   );
